41c736ef1e9eda66efc150b73c6d621135a8390b
Vovanchik15 <123307331+Vovanchik15@users.noreply.github.com>
2023-02-05 18:43:25 +0300

    Add files via upload

---
+++ hangman 2000.py
@@ -0,0 +1,83 @@
+def welcome_speech(t):
+    print(f'''
+    Добро пожаловать в игру - hangman 2000
+    Ваша задача угадать загаданное слово за несколько попыток,
+    иначе ваш ждет расплата!
+    Загаданное число состоит из {len(t)} букв {t}
+    ''')
+
+
+def check_mistake(w,g):
+    for i in w:
+        if i == g:
+            return True
+    return False
+
+
+
+
+
+
+def start_template(w):
+    return len(w)*['_']
+
+
+def list_to_string_convert(t):
+    s = ''
+    for i in t:
+        s += i
+    return s
+
+
+def user_input():
+    return input('Введите букву: ')
+
+
+def build_template(t, w, g=''):
+
+    for i in range(len(w)):
+        if w[i] == g:
+            t[i] = g
+    return t
+
+
+def get_word(w):
+    return w[0]
+
+
+def check_win(g):
+    for i in g:
+        if i == '_':
+            return True
+    return False
+
+
+def game():
+    progress = True
+    word = ['orange']
+    lifes = 3
+    
+    word_in_play = get_word(word)
+    template = start_template(word_in_play)
+    welcome_speech(list_to_string_convert(template))
+    lifes=3
+
+    while progress:
+        user_guess = user_input()
+        template = build_template(template, word_in_play, user_guess)
+        guessed = list_to_string_convert(template)
+        print(f'Результат: {guessed}')
+        progress = check_win(guessed)
+
+        if not check_mistake(word_in_play, user_guess):
+            lifes -=1
+            print(f'Осталось {lifes} попытки')
+        if lifes == 0:
+           print('Ты проиграл')
+    
+        if not progress:
+            print('Ты выиграл')
+
+
+
+game()
